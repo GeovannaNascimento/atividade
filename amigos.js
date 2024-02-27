1758c6774fad5eb16cf6b13846b134b38da5449f
Geovanna <125594657+GeovannaNascimento@users.noreply.github.com>
2024-02-27 15:13:58 -0300

    Update and rename amigos to amigos.js

--- amigos.js
+++ amigos.js
@@ -7,7 +7,7 @@ function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Carol</Text>
-      <Text style={{ marginTop: 20, textAlign: 'center' }}>Somos amigas porque nos damos bem e temos algumas coisas em comum, tipo traços de personalidade e gostos.</Text>
+      <Text style={{ marginTop: 20, textAlign: 'center' }}>Somos amigas porque nos damos bem e temos algumas coisas em comum, tipo traços de personalidade e gostos, vai imitar o relâmpago mcqueen</Text>
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
@@ -20,7 +20,7 @@ function DetailsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Lívia</Text>
-      <Text style={{ marginTop: 20, textAlign: 'center' }}>Porque nos damos bem e temos gostos parecidos.</Text>
+      <Text style={{ marginTop: 20, textAlign: 'center' }}>Porque nos damos bem e temos gostos parecidos, vai imitar um gato</Text>
       <Button
         title="Go to Details... again"
         onPress={() => navigation.push('Details')}
